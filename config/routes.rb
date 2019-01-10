62edcd4f3723443478b92459d325eaed8f65552e
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-01-10 21:29:16 +0200

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
-  # root 'books#index'
+  root 'books#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
 
-  # resources :users, only: :edit
-  # resources :categories
-  # resources :line_items
-  # resources :carts
-  # resources :books  do
-  #   resources :reviews
-  # end
+  resources :users, only: :edit
+  resources :categories
+  resources :line_items
+  resources :carts
+  resources :books  do
+    resources :reviews
+  end
 end
