e3057be80117f876642287d97bc36b3ec8376a38
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-01-25 15:16:15 +0200

    WIP with email configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,13 +97,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SENDMAIL_USERNAME'],
     password: ENV['SENDMAIL_PASSWORD'],
-    domain: 'heroku.com',
+    domain: ENV['MAIL_HOST'],
     authentication: :plain,
     enable_starttls_auto: true
   }
