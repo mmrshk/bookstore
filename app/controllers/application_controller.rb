0e6a0e450c22169e8769622c5247badf13b3c495
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-01-31 09:47:45 +0200

    Fixed saving cart after sign_in/sign_out user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,21 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
+
   def after_sign_in_path_for(resource)
+    cookies[:line_item_ids] = { value: ActiveSupport::JSON.encode(session[:line_item_ids]) }
     if cookies[:from_checkout]
       cookies.delete :from_checkout
       checkout_path(:addresses)
     else
       super
     end
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:line_item_ids] = ActiveSupport::JSON.decode(cookies[:line_item_ids])
+    super
   end
 
   private
