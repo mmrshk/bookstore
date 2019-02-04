6500af8ca6cad5f45fe349948c057e6537c087ce
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-02-04 14:53:40 +0200

    Added cart feature spec

---
+++ spec/features/cart_page_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature 'Cart page', type: :feature do
+
+  scenario 'Empty cart' do
+    visit root_path
+    find('a.shop-link.pull-right.hidden-xs').click
+
+    expect(page.current_path).to eq cart_path
+    expect(page).to have_content 'Your cart is empty'
+  end
+
+  scenario 'Full cart' do
+    FactoryBot.create(:book)
+    visit root_path
+    click_button('Buy Now')
+    find('a.shop-link.pull-right.hidden-xs').click
+
+    expect(page.current_path).to eq cart_path
+    expect(page).not_to have_content 'Your cart is empty'
+  end
+
+  scenario 'Redirect to book page' do
+    @book = FactoryBot.create(:book)
+
+    visit root_path
+    click_button('Buy Now')
+    find('a.shop-link.pull-right.hidden-xs').click
+
+    expect(page.current_path).to eq cart_path
+    expect(page).not_to have_content 'Your cart is empty'
+
+    within('.table.table-hover') do
+      find('.link_to_book').find('a').click
+    end
+
+    expect(page.current_path).to eq book_path(@book)
+  end
+end
