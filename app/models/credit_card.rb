fe13f16d4f5e685c07b7b5eed633d1de67b45d38
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-03-06 13:23:46 +0200

    Fixed checkout controller

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,7 +1,7 @@
 class CreditCard < ApplicationRecord
   REGULAR_EXPESSION = {
     name: /\A[a-zA-Z]*\s*[a-zA-Z]*\z/,
-    expiration_month_year: /((0[1-9])|(1[0-2]))[\/]*((1[5-9])|(2[0-3]))/
+    expiration_month_year: /^((0[1-9])|(1[0-2]))[\/]*((1[5-9])|(2[0-3]))$/
   }.freeze
 
   belongs_to :user, optional: true
