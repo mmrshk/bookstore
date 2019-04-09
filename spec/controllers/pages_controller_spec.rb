99f2b858744e2237126ff58c806bfcd276e73e4b
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-04-09 18:48:26 +0300

    Fixed PagesController spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   let!(:books) { create_list(:book, 5) }
 
+  let(:first_line_item) { create(:line_item, quantity: 3, book: books.first) }
+  let(:second_line_item) { create(:line_item, quantity: 2, book: books.second) }
+  let(:third_line_item) { create(:line_item, quantity: 1, book: books.third) }
+
+  let!(:order) { create(:order, line_items: [first_line_item, second_line_item, third_line_item]) }
+
   describe 'GET #home' do
     before { get :home }
 
@@ -11,7 +17,7 @@ RSpec.describe PagesController, type: :controller do
     end
 
     it 'assigns @best_sellers' do
-      expect(assigns(:best_sellers)).to be_a Array
+      expect(assigns(:best_sellers)).to eq([first_line_item.book, second_line_item.book, third_line_item.book])
     end
 
     it 'return a success response' do
--- spec/decorators/order_decorator_spec.rb
+++ spec/decorators/order_decorator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe OrderDecorator do
 
   describe '#status' do
     it 'should return status name' do
-      expect(OrderFilterService::ORDER_FILTERS[order.status.to_sym]).to eq 'Waiting for processing'
+      expect(OrderFilterService::ORDER_FILTERS[order.status.to_sym]).to eq I18n.t('models.order.in_queue')
     end
   end
 end
