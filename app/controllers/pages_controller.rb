e5d259e6cd9bc9a31e9f52e45947abf55982e568
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-11-23 16:02:55 +0200

    WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,7 @@ class PagesController < ApplicationController
     @account_info = Net::HTTP.post_form(URI.parse("https://#{@account}.joinposter.com/api/v2/auth/access_token"),
                                         params_for_request)
     @access_token = JSON.parse(@account_info.body)['access_token']
-    @waiters = JSON.parse(Net::HTTP.get_response(URI.parse("https://joinposter.com/api/access.getEmployees?token=#{@access_token}")))
+    @waiters = JSON.parse(Net::HTTP.get_response(URI.parse("https://joinposter.com/api/access.getEmployees?token=#{@access_token}")).body)
   end
 
   private
